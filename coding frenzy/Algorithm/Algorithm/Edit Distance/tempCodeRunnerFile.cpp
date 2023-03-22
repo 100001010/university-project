1a15c322d896b633f87c03d2188e8865e186f535
eric <eric93522@gmail.com>
2023-03-22 11:32:29 +0800

    up

--- coding frenzy/Algorithm/Algorithm/Edit Distance/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-freopen("in.txt","r",stdin);
-        freopen("out.txt","w",stdout);
--- coding frenzy/cpe_test/20221018cpe previous/question 1/spacil.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int n ; while(cin>> n){ 
-	n %= 9 ; if(!n) n = 9 ; 
-	cout << n << endl ; }
-}
