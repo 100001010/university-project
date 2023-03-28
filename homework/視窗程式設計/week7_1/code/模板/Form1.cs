94699400c5906068044ed8ec7312a468e35adbee
100001010 <119588517+100001010@users.noreply.github.com>
2023-03-28 10:32:21 +0800

    f

---
+++ homework/視窗程式設計/week7_1/code/模板/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 模板
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox1 = new AboutBox1();
+            aboutBox1.StartPosition = FormStartPosition.CenterScreen;
+            aboutBox1.ShowDialog();
+        }
+
+        private void label2_MouseClick(object sender, MouseEventArgs e)
+        {
+            label2.Text = "MouseClick";
+        }
+
+        private void label2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            label2.Text = "MouseDoubleClick";
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Text = "MouseDown";
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Text = "MouseEnter";
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Text = "MouseLeave";
+
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            label1.Text = "MouseUp";
+        }
+        int n = 0;
+        private void label3_MouseMove(object sender, MouseEventArgs e)
+        {
+            label3.Text = Convert.ToString(n++);
+        }
+
+        private void label4_MouseHover(object sender, EventArgs e)
+        {
+            label4.Text = "MouseHover";
+        }
+    }
+}
---
+++ homework/視窗程式設計/模板/模板/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace 模板
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(63, 23);
+            this.toolStripMenuItem1.Text = "about";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "11360465 田咏帟";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
