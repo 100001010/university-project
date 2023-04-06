d191c1f91f09283cec08ad2cb5f6a75f73d6491a
100001010 <eric93522@gmail.com>
2023-04-06 16:28:19 +0800

    T

--- homework/視窗程式設計/1_小專案/code/模板/Form1.Designer.cs
+++ homework/視窗程式設計/1_小專案/code/模板/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -47,7 +48,8 @@
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 27);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(926, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
@@ -55,16 +57,17 @@
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(63, 23);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
             this.toolStripMenuItem1.Text = "about";
             this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 30);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(279, 123);
+            this.pictureBox1.Size = new System.Drawing.Size(209, 98);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -77,25 +80,41 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(232, 390);
+            this.pictureBox2.Location = new System.Drawing.Point(174, 312);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(215, 25);
+            this.pictureBox2.Size = new System.Drawing.Size(161, 20);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(795, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "開始/重新開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(926, 487);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "11360465 田咏帟";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -112,6 +131,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- homework/視窗程式設計/1_小專案/code/模板/Form1.cs
+++ homework/視窗程式設計/1_小專案/code/模板/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -35,23 +36,86 @@ namespace 模板
         }
         int horizontal = 10;
         int vertical = 10;
+        bool start = false;
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if(pictureBox1.Left + horizontal < 0 || pictureBox1.Right + horizontal > ClientSize.Width)
+            if (start)
             {
-                horizontal = -horizontal;
+                if (pictureBox1.Left + horizontal < 0 || pictureBox1.Right + horizontal > ClientSize.Width)
+                {
+                    horizontal = -horizontal;
+                }
+                if (pictureBox1.Top + vertical < 30 )
+                {
+                    vertical = -vertical;
+                }
+
+                pictureBox1.Left += horizontal;
+                pictureBox1.Top += vertical;
             }
-            if (pictureBox1.Top + vertical < 30 || pictureBox1.Bottom + vertical > ClientSize.Height)
+            if(pictureBox1.Bottom >= pictureBox2.Bottom && pictureBox1.Left<=pictureBox2.Left && pictureBox1.Right >= pictureBox2.Right)
             {
-                vertical= -vertical;
+                vertical = -vertical + 1;
+                
+            }
+                
+            //pictureBox1.Top += vertical;
+            if(pictureBox1.Bottom + vertical > ClientSize.Height)
+            {
+                pictureBox1.Top = 100;
+                start = false;
+                    if (MessageBox.Show("再玩一次?", "你輸了", MessageBoxButtons.YesNo).ToString() == "Yes")
+                    {
+                    pictureBox1.Left= 100;
+                    
+                    horizontal = new Random().Next(0, 10);
+                    vertical = 10-horizontal;
+                    start = true;
+                    pictureBox2.Top = ClientSize.Height - 50;
+                }
+                
             }
 
-            pictureBox1.Left += horizontal;
-            pictureBox1.Top+= vertical;
         }
 
         private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
         {
+
+        }
+        int num = 30;
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Right:
+
+                    if ((pictureBox2.Left + num > 0 && pictureBox2.Right + num < ClientSize.Width))
+                        pictureBox2.Left += num;
+                    break;
+
+                case Keys.Left:
+                    if ((pictureBox2.Left - num > 0 && pictureBox2.Right - num < ClientSize.Width))
+                        pictureBox2.Left -= num;
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            horizontal = new Random().Next(0,10);
+            vertical = 10-horizontal;
+            start = true;
+            pictureBox2.Top = ClientSize.Height - 50;
 
         }
     }
